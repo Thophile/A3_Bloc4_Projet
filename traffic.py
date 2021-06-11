6c79ecaeff2ed53d7b60f4c069f7b5e9caf00729
Donovan <donovan.hoang@viacesi.fr>
2021-06-11 22:37:09 +0200

    change weight scale multiplicators

--- traffic.py
+++ traffic.py
@@ -1,4 +1,5 @@
 # Generate traffic data for an edge
 def generate():
     #return array of slowth coeficient [1-10]
-    return[1,2,5,7,4,6,9,3]
+
+    return[1, 1, 1, 1, 1, 1, 1, 2, 7, 8, 3, 2, 1, 1, 1, 2, 3, 5, 7, 5, 1, 1, 1, 1, 1]
