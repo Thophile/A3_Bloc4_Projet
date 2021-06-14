9f809ed1778c19b30225c15aebe8bd216c66752f
Thophile <montemonttheophile@gmail.com>
2021-06-14 11:54:51 +0200

    Updated printing for 3-dimension array

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ if(GENERATE):
             # Generate a graph with parameters
             matrice = Graph(n, has_traffic, is_complete, is_oriented).matrice
             if(PRINT): 
-                # Print graph using pprint
-                pprint.pprint(matrice) 
+                # Print graph using pprint , using normal print for 3 dimension array
+                print(matrice) if has_traffic else pprint.pprint(matrice) 
 
             # Generate the row that will be saved in MongoDB       
             data.append({"n" : n, "has_traffic" : has_traffic, "is_complete" : is_complete, "is_oriented" : is_oriented, "matrice" : matrice})
