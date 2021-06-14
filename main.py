37934cb9e94b904ec7f3abfdfc1480e9d7bc8aff
Thophile <montemonttheophile@gmail.com>
2021-06-14 10:57:40 +0200

    Updated generation with n range for batch generation

--- main.py
+++ main.py
@@ -14,24 +14,27 @@ db = client['DataProject']
 
 if(GENERATE):
 
-    n = 5
-    graphs_to_generate = 1
+    n_min = 5
+    n_max = 7
+    n_step = 1
+    graphs_to_generate = 2
     has_traffic = False
     is_complete = False
     is_oriented = False
 
-    data = []
-    for _ in range(graphs_to_generate):
-        # Generate a graph with parameters
-        matrice = Graph(n, has_traffic, is_complete, is_oriented).matrice
-        if(PRINT): 
-            # Print graph using pprint
-            pprint.pprint(matrice) 
+    for n in range(n_min, n_max, n_step) :
+        data = []
+        for _ in range(graphs_to_generate):
+            
+            # Generate a graph with parameters
+            matrice = Graph(n, has_traffic, is_complete, is_oriented).matrice
+            if(PRINT): 
+                # Print graph using pprint
+                pprint.pprint(matrice) 
 
-        # Generate the row that will be saved in MongoDB       
-        data.append({"n" : n, "has_traffic" : has_traffic, "is_complete" : is_complete, "is_oriented" : is_oriented, "matrice" : matrice})
-        
-    if(STORE):
-        # Save generated data into graphs collection
-        graphs = db['graphs']
-        graphs.insert_many(list(data))
+            # Generate the row that will be saved in MongoDB       
+            data.append({"n" : n, "has_traffic" : has_traffic, "is_complete" : is_complete, "is_oriented" : is_oriented, "matrice" : matrice})
+        if(STORE):
+            # Save generated data into graphs collection
+            graphs = db['graphs']
+            graphs.insert_many(list(data))
