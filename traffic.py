cad1ab7773f90cdda50a071d0e0f2154c0583d3f
Donovan <donovan.hoang@viacesi.fr>
2021-06-14 09:42:54 +0200

    put accident in traffic

--- traffic.py
+++ traffic.py
@@ -1,5 +1,11 @@
 # Generate traffic data for an edge
+import random
 def generate():
     #return array of slowth coeficient [1-10]
-    
-    return[1, 1, 1, 1, 1, 1, 1, 2, 7, 8, 3, 2, 1, 1, 1, 2, 3, 5, 7, 5, 1, 1, 1, 1, 1]
+    traffic = [1, 1, 1, 1, 1, 1, 1, 2, 7, 8, 3, 2, 1, 1, 1, 2, 3, 5, 7, 5, 1, 1, 1, 1, 1]
+
+    for weight in traffic:
+        if(random.randrange(1,1000) < 28):
+            weight += 1
+
+    return traffic
