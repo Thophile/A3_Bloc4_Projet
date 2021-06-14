49811aa99b8c11940d59205eb29a794dd5b720c8
Thophile <montemonttheophile@gmail.com>
2021-06-14 11:08:18 +0200

    Updated livrable 2 - etape 2 comments

--- graph.py
+++ graph.py
@@ -17,7 +17,8 @@ class Graph:
                     weight = g[j][i]
                 else:
                     weight = Edge(
-                        # Random base weight for the edge, 0 if the edge is a loop
+                        # Random base weight for the edge, range start at 1 if the graphs is complete,
+                        # weight is 0 if the edge is a loop
                         random.randrange((1 if is_complete else 0),10) if i!=j else 0,
                         # Generated traffic data or 1 if traffic is not used
                         traffic.generate() if has_traffic else [1] 
