fc3bb3592e980dd33d78ceef843509fb1f18d589
Donovan <donovan.hoang@viacesi.fr>
2021-06-14 14:05:29 +0200

    fix 1 to 5 in gen

--- graph.py
+++ graph.py
@@ -19,7 +19,7 @@ class Graph:
                     weight = Edge(
                         # Random base weight for the edge, range start at 1 if the graphs is complete,
                         # weight is 0 if the edge is a loop
-                        random.randrange((1 if is_complete else 0),240, 5) if i!=j else 0,
+                        random.randrange((5 if is_complete else 0),240, 5) if i!=j else 0,
                         # Generated traffic data or 1 if traffic is not used
                         generate() if has_traffic else [0] 
                         ).weight
