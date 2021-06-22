2edcb895a81ea3804f3ec4198a67ddc5f58ea43b
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2021-06-22 12:50:47 +0200

    added stats and turned to vrp pb

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from graph import Graph 
-from heuristic import heuristic
+from heuristic import create_tour
 from pymongo import MongoClient
+import matplotlib.pyplot as plt
 import pprint
 import random
+import time
 
 # Constants
-FLUSH = True
-GENERATE = True
+FLUSH = False
+GENERATE = False
 PRINT = False
 SEARCH = True
+STATS = True
 
 # Connection to MongoDB
 client = MongoClient('localhost', 27017)
@@ -19,16 +22,17 @@ if(FLUSH):
     graphs.delete_many({})
 if(GENERATE):
 
-    n_min = 2000
-    n_max = 2001
+    n_min = 10
+    n_max = 21
     n_step = 1
-    graphs_to_generate = 1
+    graph_per_size = 10
     has_traffic = True
     is_complete = True
     is_oriented = False
 
+    graph_id = 0
     for n in range(n_min, n_max, n_step) :
-        for _ in range(graphs_to_generate):
+        for _ in range(graph_per_size):
             
             # Generate a graph with parameters
             matrice = Graph(n, has_traffic, is_complete, is_oriented).matrice
@@ -49,18 +53,41 @@ if(GENERATE):
                 end_time = start_time+time_window
 
                 # Save generated data into graphs collection
-                graphs.insert_one( {"graph_id" : _ , "node" : node, "start_time" : start_time, "end_time" : end_time, "n" : n, "has_traffic" : has_traffic, "is_complete" : is_complete, "is_oriented" : is_oriented, "row" : matrice[node]})
+                graphs.insert_one( {"graph_id" : graph_id , "node" : node, "start_time" : start_time, "end_time" : end_time, "n" : n, "has_traffic" : has_traffic, "is_complete" : is_complete, "is_oriented" : is_oriented, "row" : matrice[node]})
+            graph_id += 1
     # Rows check
     print("Rows : "+str(graphs.count_documents({})))
 
 if(SEARCH):
     # Search optimum route
-    graphs_to_search = 1
-    iter_max = 3000
+    graphs_ids = graphs.find_one({}, {"_id":0, "graph_id":1}, sort=[("graph_id", -1)])["graph_id"]+1
+    print(graphs_ids)
+    iter_max = 10
     level_max = 10
+    vehicules_nb = 4
 
-    for graph_id in range(graphs_to_search) :
-
+    if STATS : 
+        times = []
+    for graph_id in range(graphs_ids) :
+        if STATS : 
+            size = graphs.find_one({"graph_id" : graph_id}, {"_id":0, "n":1})["n"]
+            start = time.time()
         # Getting a row for verbal param output
         heuristic(graph_id, iter_max, level_max)
-        # best current : 4500
+        if STATS : 
+            duration = time.time() - start
+            times.append({"time" : duration, "size" : size})
+    if STATS :
+        sizes = []
+        avg_times = []
+        tmp = {}
+        for entry in times:
+            if not entry["size"] in tmp : tmp[entry["size"]] = []
+            tmp[entry["size"]].append(entry["time"])
+        for key, value in tmp.items():
+            sizes.append(key)
+            avg_times.append(sum(value)/len(value))
+        plt.plot(sizes, avg_times)
+        plt.ylabel('time')
+        plt.xlabel('graph size')
+        plt.show()
