0b6171edab86d19458517d68369bad351109c4d9
Théophile MONTÉMONT <44491166+Thophile@users.noreply.github.com>
2021-06-28 15:46:06 +0200

    added weighted array génération from stats function

--- traffic.py
+++ traffic.py
@@ -1,15 +1,24 @@
 import random
+import math
 
 # Generate traffic data for an edge
 def generate():
-    
-    # Base weighted array
-    traffic = [1, 1, 1, 1, 1, 1, 1, 2, 7, 8, 3, 2, 1, 1, 1, 2, 3, 5, 7, 5, 1, 1, 1, 1, 1]
+    m_a = 1503.43402
+    m_b = -556561.2208
+    m_max = m_a*60*11+m_b
 
-    # Random weight corresponding to crash probability
-    for i in range (0, len(traffic)):
-        if(random.randrange(1,1000) < 28):
-            traffic[i] += 1
-    
+    e_a = -1507.393614
+    e_b = 1862163.515
+    e_max = e_a*60*12+e_b
+    morning = [int(round(10*(m_a*60*i+m_b)/m_max)) if 10*(m_a*60*i+m_b)/m_max >= 0 else 0 for i in range(0,12) ]
+    evening = [int(round(10*(e_a*60*i+e_b)/e_max)) if 10*(e_a*60*i+e_b)/e_max >=0 else 0 for i in range(12,24)]
+
+    traffic=morning+evening
+    # Predicted weight array from project given data
+    # traffic = [0, 0, 0, 0, 0, 0, 0, 2, 4, 6, 8, 10, 10, 9, 8, 7, 5, 4, 3, 2, 1, 0, 0, 0]
+
+    # Base weighted array from offical french stats
+    # traffic = [1, 1, 1, 1, 1, 1, 1, 2, 7, 8, 3, 2, 1, 1, 1, 2, 3, 5, 7, 5, 1, 1, 1, 1, 1]
+
     # Return array of slowth coeficient [1-10]
     return traffic
