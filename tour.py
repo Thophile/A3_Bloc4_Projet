fc290fcb4f6a6f8ce30cd565d0a3efc3a06874e8
Thophile <montemonttheophile@gmail.com>
2021-06-29 16:13:15 +0200

    renamed i to node for clarity

--- tour.py
+++ tour.py
@@ -25,16 +25,16 @@ def create_tour(params, graph_info, iter, level, vehicules_nb, depot=0, callback
     for _ in range(vehicules_nb):
         tours.append([])
     # for each node
-    for i in range(len(graph)):
+    for node in range(len(graph)):
         # put it in a random tour if it is not the depot
-        if i != depot :
+        if node != depot :
             #ensure every tour as at least 1 node
             index = random.randrange(0,vehicules_nb)
             for _ in range(vehicules_nb):
                 if len(tours[_]) == 0 :
                     index = _
                     break
-            tours[index].append(i)
+            tours[index].append(node)
         
     if DEBUG : 
         print("Graph loaded & Tours generated : ")
