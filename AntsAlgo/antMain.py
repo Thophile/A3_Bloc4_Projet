4b453b7214883cf1fba34785d0fd4470775c6047
Donovan <donovan.hoang@viacesi.fr>
2021-07-01 17:44:10 +0200

    comment ant main

--- AntsAlgo/antMain.py
+++ AntsAlgo/antMain.py
@@ -8,7 +8,7 @@ def antAlgo(param, graph, tw, tour, iter_max, level):
     ants = list() # list of ants
     starting_node = 0 #  node from where the ants
     for i in range(nb_fourmis):
-        ants.append(Ant())  
+        ants.append(Ant())  # instanciate ants
     phero = [] # the amount of pheromones in each node of the graph
 
     for i in range(len(graph)):
@@ -17,34 +17,34 @@ def antAlgo(param, graph, tw, tour, iter_max, level):
             node.append(10)
         phero.append(node)
     
-    verif = 0
-    it= 0
-    while True and it < 150:
+    verif = 0 # counter of convergence
+    it= 0 # number of iteration
+    while True and it < 150: # iterate for 150 iterations
         it += 1
-        for ant in ants:
+        for ant in ants: # initialize the ants
             ant.toVisit = list.copy(tour)
             ant.visited = [starting_node]
-            ant.travel(graph, phero, tw, param)
+            ant.travel(graph, phero, tw, param) # start the travel of the ants
 
-        for i in range(len(phero)):         #Evaportaion
+        for i in range(len(phero)):         #Evaportaion of the pheromones
             for j in range(len(phero)):
                 phero[i][j] = phero[i][j] * RHO
 
         for ant in ants :
-            phero = ant.spittingPheromone(phero, graph, param)
+            phero = ant.spittingPheromone(phero, graph, param) # deposit of pheromones
 
-        if DEBUG : print(verif,tour,ants[0].visited)
+        if DEBUG : print(verif,tour,ants[0].visited) # debug
 
         same = True
-        for ant in ants:
+        for ant in ants: # check if all the ants have the same solution
             if ants[0].visited != ant.visited:
                 same = False
         if same:
             verif += 1
         else : verif = 0
-        if verif >= 3:
+        if verif >= 3: # verif of the ants have the same solution for 3 times in a row
             break
-    solution = list.copy(ants[0].visited)
+    solution = list.copy(ants[0].visited) # get the solution (array of city)
     solution.pop(0)
     solution.pop(len(solution)-1)
     return solution
